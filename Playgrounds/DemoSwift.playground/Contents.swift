84268eeca2677c982176cf8991d42c3aad23a57b
Ludovic Ollagnier <ludovic@ollagnier.me>
2021-04-16 11:05:22 +0200

    Added data types slides and demo

---
+++ Playgrounds/DemoSwift.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// Stock
+let name = "AAPL" // Constante
+var value = 134.50 // Variable
+
+//value = "trente"
+//name = "trente"
+
+// Explicit types
+let userName: String = "Ludovic"
+let id: Int = 12
+var lastLogin: Date = Date()
+
+var score: Double = 0 // Int
+
+let a = 5
+let b = 2
+let c = a / b
+let preciseC = Double(a) / Double(b)
+// alt (option) clic sur le nom
+
+print(c)
+
