bda7d23af0895f80b09f20ffd46f16c9af7a21b1
Ludovic Ollagnier <ludovic@ollagnier.me>
2021-04-16 19:07:48 +0200

    Class app model demo

--- Playgrounds/App Model.playground/Contents.swift
+++ Playgrounds/App Model.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 // Nom des types est capitalisé
-struct Meal {
+struct Meal: Equatable {
 
     // Nom des propriétés commence par minuscule
     let name: String
@@ -12,10 +12,37 @@ struct Meal {
     let isVeggie: Bool
 }
 
-struct Ingredient {
-
+struct Ingredient: Equatable {
     let name: String
     let isVeggie: Bool
 }
 
 let rice = Ingredient(name: "Rice", isVeggie: true)
+
+class Restaurant {
+
+    let name: String
+    let address:String
+    var meals: [Meal]
+    var mediumMealPrice: Double
+    let pitch: String
+
+    init(name: String, address: String, pitch: String, meals: [Meal] = []) {
+        self.name = name
+        self.address = address
+        self.pitch = pitch
+        self.meals = meals
+        mediumMealPrice = 0 // to calculate
+    }
+
+    func add(_ meal: Meal) {
+        meals.append(meal)
+    }
+
+    func remove(_ meal: Meal) {
+        if let mealIndex = meals.firstIndex(of: meal) {
+            meals.remove(at: mealIndex)
+        }
+//        meals.removeAll(where: <#T##(Meal) throws -> Bool#>)
+    }
+}
